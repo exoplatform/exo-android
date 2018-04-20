aaa47cbca385dafe56379b059ec782b893f86135
Thomas Delhoménie <tdelhomenie@exoplatform.com>
2018-04-20 10:22:00 +0200

    Make the notification clickable

--- app/src/main/java/org/exoplatform/service/push/PushNotificationsService.java
+++ app/src/main/java/org/exoplatform/service/push/PushNotificationsService.java
@@ -20,9 +20,13 @@ package org.exoplatform.service.push;
  *
  */
 
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Build;
 import android.service.notification.StatusBarNotification;
 import android.support.v4.app.NotificationCompat;
@@ -34,6 +38,8 @@ import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
 import org.exoplatform.R;
+import org.exoplatform.activity.ConnectServerActivity;
+import org.exoplatform.activity.WebViewActivity;
 
 public class PushNotificationsService extends FirebaseMessagingService {
 
@@ -70,10 +76,18 @@ public class PushNotificationsService extends FirebaseMessagingService {
       contentText = Html.fromHtml(messageBody);
     }
 
+    Intent notificationIntent = new Intent(this, ConnectServerActivity.class);
+
+    PendingIntent pendingIntent =
+            TaskStackBuilder.create(this)
+                    .addNextIntent(notificationIntent)
+                    .getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
     NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, channelId)
               .setSmallIcon(R.drawable.icon_share_notif)
               .setContentTitle(messageTitle)
               .setContentText(contentText)
+              .setContentIntent(pendingIntent)
               .setStyle(new NotificationCompat.BigTextStyle().bigText(contentText))
               .setAutoCancel(true);
 
