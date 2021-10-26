02a2d770899a8fb2bed6c22182ebbd204b8e2192
Wajih Benabdessalem <wbenabdessalem@exoplatform.com>
2021-10-26 15:45:40 +0100

    remove unwanted changes

--- app/src/main/java/org/exoplatform/activity/SliderAdapter.java
+++ app/src/main/java/org/exoplatform/activity/SliderAdapter.java
@@ -5,11 +5,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+
 import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.viewpager.widget.PagerAdapter;
+
 import com.bumptech.glide.Glide;
+
 import org.exoplatform.R;
+
 import java.util.Locale;
 
 public class SliderAdapter extends PagerAdapter {
