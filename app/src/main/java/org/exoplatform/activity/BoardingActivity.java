1b36d02def3983ac8d4160f18b66da90dd1638d9
Wajih Benabdessalem <wbenabdessalem@exoplatform.com>
2021-10-26 17:58:58 +0100

    49934 : Fix Problem when opening the qr camera for first time

--- app/src/main/java/org/exoplatform/activity/BoardingActivity.java
+++ app/src/main/java/org/exoplatform/activity/BoardingActivity.java
@@ -2,22 +2,31 @@ package org.exoplatform.activity;
 
 import static org.exoplatform.activity.WebViewActivity.INTENT_KEY_URL;
 
+import android.Manifest;
+import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.viewpager.widget.ViewPager;
 import com.google.android.material.tabs.TabLayout;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
+import com.journeyapps.barcodescanner.CaptureManager;
+
 import org.exoplatform.App;
 import org.exoplatform.BuildConfig;
 import org.exoplatform.R;
@@ -35,6 +44,7 @@ public class BoardingActivity extends AppCompatActivity {
     private TextView slideTitle;
     private TextView currentPage;
     private TextView scanQRBtn;
+    int PERMISSION_CAMERA = 0;
 
     LinearLayout scanQRFragmentBtn;
     TextView enterServerFragmentBtn;
@@ -79,9 +89,8 @@ public class BoardingActivity extends AppCompatActivity {
             @RequiresApi(api = Build.VERSION_CODES.M)
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(BoardingActivity.this,ScannerActivity.class);
-                intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
-                startActivityForResult(intent,REQUEST_CODE);
+                Activity context = BoardingActivity.this;
+                context.requestPermissions(new String[]{Manifest.permission.CAMERA}, 1011);
             }
         });
 
@@ -110,6 +119,23 @@ public class BoardingActivity extends AppCompatActivity {
     @Override
     public void onBackPressed() {
 
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        if (requestCode == 1011) {// If request is cancelled, the result arrays are empty.
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Intent intent = new Intent(BoardingActivity.this, ScannerActivity.class);
+                intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+                intent.putExtra("SCAN_CAMERA_ID", 0);
+                startActivityForResult(intent,REQUEST_CODE);
+            } else {
+
+                // permission denied, boo! Disable the
+                // functionality that depends on this permission.
+                Toast.makeText(BoardingActivity.this, "Permission denied to read your Camera", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     @Override
--- app/src/main/java/org/exoplatform/activity/ScannerActivity.java
+++ app/src/main/java/org/exoplatform/activity/ScannerActivity.java
@@ -1,8 +1,11 @@
 package org.exoplatform.activity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.media.AudioManager;
 import android.media.ToneGenerator;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.KeyEvent;
@@ -11,8 +14,10 @@ import android.view.animation.Animation;
 import android.view.animation.ScaleAnimation;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.zxing.ResultPoint;
@@ -37,8 +42,8 @@ public class ScannerActivity extends AppCompatActivity {
     ImageView qr_code_imageView;
     ViewfinderView viewFinder;
     private Handler handler;
-    int PERMISSION_CAMERA = 0;
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -93,12 +98,6 @@ public class ScannerActivity extends AppCompatActivity {
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         return barcodeScannerView.onKeyDown(keyCode, event) || super.onKeyDown(keyCode, event);
     }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        capture.onRequestPermissionsResult(requestCode, permissions, grantResults);
-    }
-
 
     BarcodeCallback callback = new BarcodeCallback() {
         @Override
