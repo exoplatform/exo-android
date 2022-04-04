426307ff009f36380fb34898798e8aa0ce0be29a
Wajih Benabdessalem <wbenabdessalem@exoplatform.com>
2022-04-04 10:00:57 +0100

    55852 : Download file from Activity stream activity

---
+++ app/src/main/java/org/exoplatform/tool/JavaScriptInterface.java
@@ -0,0 +1,72 @@
+package org.exoplatform.tool;
+
+import android.content.Context;
+import android.os.Environment;
+import android.util.Base64;
+import android.util.Log;
+import android.webkit.JavascriptInterface;
+import android.webkit.MimeTypeMap;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.util.Date;
+
+public class JavaScriptInterface {
+    private static String fileMimeType;
+    private Context context;
+    public JavaScriptInterface(Context context) {
+        this.context = context;
+    }
+
+    @JavascriptInterface
+    public void getBase64FromBlobData(String base64Data) throws IOException {
+        convertBase64StringToPdfAndStoreIt(base64Data);
+    }
+
+    public static String getBase64StringFromBlobUrl(String blobUrl,String mimeType) {
+        if(blobUrl.startsWith("blob")){
+            fileMimeType = mimeType;
+            return "javascript: var xhr = new XMLHttpRequest();" +
+                    "xhr.open('GET', '"+ blobUrl +"', true);" +
+                    "xhr.setRequestHeader('Content-type','" + mimeType +";charset=UTF-8');" +
+                    "xhr.responseType = 'blob';" +
+                    "xhr.onload = function(e) {" +
+                    "    if (this.status == 200) {" +
+                    "        var blobFile = this.response;" +
+                    "        var reader = new FileReader();" +
+                    "        reader.readAsDataURL(blobFile);" +
+                    "        reader.onloadend = function() {" +
+                    "            base64data = reader.result;" +
+                    "            Android.getBase64FromBlobData(base64data);" +
+                    "        }" +
+                    "    }" +
+                    "};" +
+                    "xhr.send();";
+        }
+        return "javascript: console.log('It is not a Blob URL');";
+    }
+    private void convertBase64StringToPdfAndStoreIt(String base64PDf) throws IOException {
+        String currentDateTime = DateFormat.getDateTimeInstance().format(new Date());
+        String newTime = currentDateTime.replaceFirst(", ","_").replaceAll(" ","_").replaceAll(":","-");
+        Log.d("fileMimeType ====> ",fileMimeType);
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        String extension = mimeTypeMap.getExtensionFromMimeType(fileMimeType);
+        final File dwldsPath = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DOWNLOADS) + "/" + newTime + "_." + extension);
+        String regex = "^data:" + fileMimeType + ";base64,";
+        byte[] pdfAsBytes = Base64.decode(base64PDf.replaceFirst(regex, ""), 0);
+        try {
+            FileOutputStream os = new FileOutputStream(dwldsPath);
+            os.write(pdfAsBytes);
+            os.flush();
+            os.close();
+        } catch (Exception e) {
+            Toast.makeText(context, "FAILED TO DOWNLOAD THE FILE!", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+        Toast.makeText(context, "FILE DOWNLOADED!", Toast.LENGTH_SHORT).show();
+    }
+}
