18c0f2d01598dc43911cf701c19af1ecf012d2d0
wbenabdessalem <82816251+wbenabdessalem@users.noreply.github.com>
2022-04-11 11:05:10 +0100

    Backport w15 (#115)
    * Task-55247 : When downloading a file from the activity stream, the Exo app is closed automatically (#109)
    
    * 55247 : When downloading a file from the activity stream, the Exo app is closed automatically
    
    * Update on task
    
    * Task-55852 : Download file from Activity stream activity (#110)
    
    * 55852 : Download file from Activity stream activity
    
    * Task-55852 :Update, Add Licence
    
    * Task-55852 : Add Licience AGPL
    
    * Task-55636 : Add notif download (#111)
    
    * Initialize CI/CD for acceptance environment
    
    * 55247 : When downloading a file from the activity stream, the Exo app is closed automatically (#108)
    
    * Task-55247 : When downloading a file from the activity stream, the Exo app is closed automatically (#109)
    
    * 55247 : When downloading a file from the activity stream, the Exo app is closed automatically
    
    * Update on task
    
    * 55852 : Download file from Activity stream activity
    
    * 55636 : Use native system when i download a document
    
    * Task-55636 : Update ,add licence
    
    * Update app/src/main/java/org/exoplatform/tool/JavaScriptInterface.java
    
    Co-authored-by: Romain Dénarié <romain.denarie@exoplatform.com>
    
    * Task-55636 : Add Licence AGPL
    
    * Task-55852 : Download file from Activity stream activity (#110)
    
    * 55852 : Download file from Activity stream activity
    
    * Task-55852 :Update, Add Licence
    
    * Task-55852 : Add Licience AGPL
    
    * Update app/src/main/AndroidManifest.xml
    
    Co-authored-by: Romain Dénarié <romain.denarie@exoplatform.com>
    
    * Update app/src/main/AndroidManifest.xml
    
    Co-authored-by: Romain Dénarié <romain.denarie@exoplatform.com>
    
    * Update app/src/main/AndroidManifest.xml
    
    Co-authored-by: Romain Dénarié <romain.denarie@exoplatform.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Houssem Ben Ali <hbenali@exoplatform.com>
    Co-authored-by: Romain Dénarié <romain.denarie@exoplatform.com>
    
    Co-authored-by: Houssem Ben Ali <hbenali@exoplatform.com>
    Co-authored-by: Romain Dénarié <romain.denarie@exoplatform.com>

---
+++ app/src/main/java/org/exoplatform/tool/JavaScriptInterface.java
@@ -0,0 +1,118 @@
+package org.exoplatform.tool;
+
+/*
+ * Copyright (C) 2022 eXo Platform SAS.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.util.Base64;
+import android.util.Log;
+import android.webkit.JavascriptInterface;
+import android.webkit.MimeTypeMap;
+import android.widget.Toast;
+import androidx.core.content.FileProvider;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.util.Date;
+
+public class JavaScriptInterface {
+    private static String fileMimeType;
+    private Context context;
+    public JavaScriptInterface(Context context) {
+        this.context = context;
+    }
+
+    @JavascriptInterface
+    public void getBase64FromBlobData(String base64Data) throws IOException {
+        convertBase64StringToPdfAndStoreIt(base64Data);
+    }
+
+    public static String getBase64StringFromBlobUrl(String blobUrl,String mimeType) {
+        if(blobUrl.startsWith("blob")){
+            fileMimeType = mimeType;
+            return "javascript: var xhr = new XMLHttpRequest();" +
+                    "xhr.open('GET', '"+ blobUrl +"', true);" +
+                    "xhr.setRequestHeader('Content-type','" + mimeType +";charset=UTF-8');" +
+                    "xhr.responseType = 'blob';" +
+                    "xhr.onload = function(e) {" +
+                    "    if (this.status == 200) {" +
+                    "        var blobFile = this.response;" +
+                    "        var reader = new FileReader();" +
+                    "        reader.readAsDataURL(blobFile);" +
+                    "        reader.onloadend = function() {" +
+                    "            base64data = reader.result;" +
+                    "            Android.getBase64FromBlobData(base64data);" +
+                    "        }" +
+                    "    }" +
+                    "};" +
+                    "xhr.send();";
+        }
+        return "javascript: console.log('It is not a Blob URL');";
+    }
+    private void convertBase64StringToPdfAndStoreIt(String base64PDf) throws IOException {
+        final int notificationId = 1;
+        String currentDateTime = DateFormat.getDateTimeInstance().format(new Date());
+        String newTime = currentDateTime.replaceFirst(", ","_").replaceAll(" ","_").replaceAll(":","-");
+        Log.d("fileMimeType ====> ",fileMimeType);
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        String extension = mimeTypeMap.getExtensionFromMimeType(fileMimeType);
+        final File dwldsPath = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DOWNLOADS) + "/" + newTime + "_." + extension);
+        String regex = "^data:" + fileMimeType + ";base64,";
+        byte[] pdfAsBytes = Base64.decode(base64PDf.replaceFirst(regex, ""), 0);
+        try {
+            FileOutputStream os = new FileOutputStream(dwldsPath);
+            os.write(pdfAsBytes);
+            os.flush();
+            os.close();
+        } catch (Exception e) {
+            Toast.makeText(context, "FAILED TO DOWNLOAD THE FILE!", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+        if (dwldsPath.exists()) {
+            Intent intent = new Intent();
+            intent.setAction(android.content.Intent.ACTION_VIEW);
+            Uri apkURI = FileProvider.getUriForFile(context,context.getApplicationContext().getPackageName() + ".provider", dwldsPath);
+            intent.setDataAndType(apkURI, MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension));
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context,1, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+            String CHANNEL_ID = "MYCHANNEL";
+            final NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            NotificationChannel notificationChannel= new NotificationChannel(CHANNEL_ID,"name", NotificationManager.IMPORTANCE_LOW);
+            Notification notification = new Notification.Builder(context,CHANNEL_ID)
+                    .setContentText("You have got something new!")
+                    .setContentTitle("File downloaded")
+                    .setContentIntent(pendingIntent)
+                    .setChannelId(CHANNEL_ID)
+                    .setSmallIcon(android.R.drawable.stat_sys_download_done)
+                    .build();
+            if (notificationManager != null) {
+                notificationManager.createNotificationChannel(notificationChannel);
+                notificationManager.notify(notificationId, notification);
+            }
+        }
+        Toast.makeText(context, "FILE DOWNLOADED!", Toast.LENGTH_SHORT).show();
+    }
+}
