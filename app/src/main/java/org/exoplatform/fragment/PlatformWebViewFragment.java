71014967501e9576a66639157ba9df7b7686cabe
wbenabdessalem <82816251+wbenabdessalem@users.noreply.github.com>
2022-10-07 10:32:13 +0100

    Task-59859: Mobile_Android_jitsi_join a call_US01 (#141)

--- app/src/main/java/org/exoplatform/fragment/PlatformWebViewFragment.java
+++ app/src/main/java/org/exoplatform/fragment/PlatformWebViewFragment.java
@@ -256,6 +256,7 @@ public class PlatformWebViewFragment extends Fragment {
                   }
                 }
               }
+              switchToJitsiAppWith(url);
               return false;
             }
             @Override
