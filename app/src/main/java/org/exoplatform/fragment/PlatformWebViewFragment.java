e37ad1e3962a2390a22083fd2716daae0f13eab2
wbenabdessalem <82816251+wbenabdessalem@users.noreply.github.com>
2022-11-12 14:13:49 +0100

    Task-60655: Mobile_Android_Visioconference_Task ; Join a call on mobile (#146)

--- app/src/main/java/org/exoplatform/fragment/PlatformWebViewFragment.java
+++ app/src/main/java/org/exoplatform/fragment/PlatformWebViewFragment.java
@@ -286,7 +286,6 @@ public class PlatformWebViewFragment extends Fragment {
                   }
                  }
               }
-              switchToJitsiAppWith(url);
               return false;
             }
             @Override
