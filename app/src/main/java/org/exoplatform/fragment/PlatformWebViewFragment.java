dc9bb9d7b3b42c94a5359c4659e8357c26ffdea3
wbenabdessalem <82816251+wbenabdessalem@users.noreply.github.com>
2023-02-03 17:26:05 +0100

    Backport w05 (#152)
    * Task-59859: Mobile_Android_jitsi_join a call_US01 (#141)
    
    (cherry picked from commit 346f21a004f6040c0dd66bc0b3d8030b15488dff)
    
    * Task-60656: Android Quiting jitsi call blocks eXo app (#144)
    
    * Task-60656: Android Quiting jitsi call blocks eXo app
    
    * Task-60656: improve imports
    
    (cherry picked from commit 4802cb944992a3e8e2b72986ed98b13d06698a77)
    
    * Task-60655: Mobile_Android_Visioconference_Task ; Join a call on mobile (#146)
    
    (cherry picked from commit f768fdb4bded74263d019dcd1f53d38c4bfc6ca8)
    
    * Task-60337: Open an external link do not open the browser (#147)
    
    (cherry picked from commit 6b3adf3693e443f46ed2a76ca3da54bde887ffb6)
    
    * Task-60802: Mobile_external link_Tech, Fix warning google play console (#148)
    
    (cherry picked from commit 1ec41f8a5cf045541b6d0574fada00a1f2fb4dc7)
    
    * Task-60896: call from jitsi app send a notification on exo (#149)
    
    (cherry picked from commit cab05b74d18b8247a5f3198d4315dc0e6794cff7)
    
    * Task-60337: Mobile_Android_BUG: Open an external link do not open the browser (#150)
    
    (cherry picked from commit 13074702b2537b57bfba56c3fa3761f8811a763b)
    
    * Task-60656: Add permission to the jitsi App (#151)
    
    (cherry picked from commit cf89c69e9dc5c181772898f295098832b7e71cfb)
    
    * Update Java version to 11
    
    ---------
    
    Co-authored-by: Ali HAMDI <ahamdi@exoplatform.com>

--- app/src/main/java/org/exoplatform/fragment/PlatformWebViewFragment.java
+++ app/src/main/java/org/exoplatform/fragment/PlatformWebViewFragment.java
@@ -20,10 +20,15 @@ package org.exoplatform.fragment;
  *
  */
 
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+import static android.content.Context.DOWNLOAD_SERVICE;
+import static org.exoplatform.activity.WebViewActivity.INTENT_KEY_URL;
+
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.DownloadManager;
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -43,6 +48,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.webkit.ConsoleMessage;
 import android.webkit.CookieManager;
 import android.webkit.DownloadListener;
 import android.webkit.JsResult;
@@ -51,6 +57,7 @@ import android.webkit.PermissionRequest;
 import android.webkit.URLUtil;
 import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
+import android.webkit.WebResourceError;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebResourceResponse;
 import android.webkit.WebSettings;
@@ -64,6 +71,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import org.exoplatform.App;
@@ -94,10 +102,6 @@ import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
-
-import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
-import static android.content.Context.DOWNLOAD_SERVICE;
-import static org.exoplatform.activity.WebViewActivity.INTENT_KEY_URL;
 
 /**
  * WebView that is configured to display content from a Platform 4.3+ intranet.
@@ -223,22 +227,50 @@ public class PlatformWebViewFragment extends Fragment {
                     newWebView.getWebChromeClient().onCloseWindow(view);
                   }
               }
-              if (path.equals("/portal/dw/news/editor")) {
-                mWebView.loadUrl(url);
-                newWebView.getWebChromeClient().onCloseWindow(view);
+              if (!url.contains(mServer.getShortUrl()) || path.equals("/portal/dw/news/editor")) {
+                if (!url.contains(mServer.getShortUrl())) {
+                  view.getContext().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+                  newWebView.getWebChromeClient().onCloseWindow(view);
+                  return true;
+                }else{
+                  mWebView.loadUrl(url);
+                  newWebView.getWebChromeClient().onCloseWindow(view);
+                }
               }
               if (url.contains("/jitsi/meet")) {
                 // Set permission to Camera and micro to be enabled for the Jitsi call.
                 String[] permissions = { Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.INTERNET,Manifest.permission.RECORD_AUDIO,
                         Manifest.permission.CAMERA };
-                ActivityCompat.requestPermissions(Objects.requireNonNull(getActivity()),permissions,1010);
+                for (String permission : permissions) {
+                  if (ContextCompat.checkSelfPermission(getActivity(), permission) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(getActivity(), permissions, 1010);
+                    break;
+                  }
+                }
               }
               return false;
+            }
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+              super.onReceivedError(view, request, error);
+              Log.d("onReceivedError ========< ",error.toString());
+              if (String.valueOf(error.getDescription()).contains("ERR_UNKNOWN_URL_SCHEME")) {
+                newWebView.getWebChromeClient().onCloseWindow(view);
+              }
             }
           });
           //setDownloadListenerFor(newWebView,getContext());
           // Set the created window to be closed automatically when we have Jitsi call or google sign in (related to JS window.close()).
           newWebView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onConsoleMessage(ConsoleMessage consoleMessage) {
+              String url = consoleMessage.sourceId();
+              switchToJitsiAppWith(url);
+              if (url.contains("Call started")) {
+                newWebView.getWebChromeClient().onCloseWindow(newWebView);
+              }
+              return true;
+            }
             @Override
             public void onCloseWindow(WebView window) {
               super.onCloseWindow(window);
@@ -308,6 +340,21 @@ public class PlatformWebViewFragment extends Fragment {
       });
     }
     return layout;
+  }
+
+  public void switchToJitsiAppWith(String url){
+    if (url.contains("/jitsiweb/") && url.contains("?jwt=") && !url.contains("intent://")){
+      String fUrl = url.replaceFirst("^(http[s]?://)","");
+      Uri uri = Uri.parse("org.jitsi.meet://" + fUrl);
+      Intent jitsiURL = new Intent(Intent.ACTION_VIEW, uri);
+      jitsiURL.setPackage("org.jitsi.meet");
+      try {
+        startActivity(jitsiURL);
+        newWebView.getWebChromeClient().onCloseWindow(newWebView);
+      } catch (ActivityNotFoundException e) {
+        Log.e("Jitsi App not found:", e.toString());
+      }
+    }
   }
 
   public static String getMimeType(String url) {
@@ -557,6 +604,10 @@ public class PlatformWebViewFragment extends Fragment {
 
       if (((url.contains(mServer.getShortUrl()) && !super.shouldOverrideUrlLoading(view, request))) || isWhileLoginProcess) {
         // url is on the server's domain, keep loading normally
+        if (url.contains("/jitsi/meet")) {
+          mListener.onExternalContentRequested(url);
+          return true;
+        }
         return false;
       } else{
           // url is on an external domain, load in a different fragment
