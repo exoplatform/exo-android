6ae4a609230b7d4cfa45207ec178c33d957fa1b9
Houssem Ben Ali <hbenali@exoplatform.com>
2024-04-24 16:41:55 +0200

    Bump to SDK 34 (Android 14) + Gradle to 8.7 + JDK to 17 + GHAs - EXO-71305 - EXO-71305 (#184)
    * Bump to SDK 34 (Android 14) + Gradle to 8.7 + JDK to 17 + GHAs - EXO-71305 - EXO-71305
    
    This fiw will :
     - Upgrade to use Android SDK 34 (Android 14)
     - Upgrade Gradle to 8.7
    - Upgrade Java to 17
     - Set min version to install Android app on to  Android 11
    
    * fix build failure (#183)
    
    ---------
    
    Co-authored-by: Ali HAMDI <ahamdi@exoplatform.com>

--- app/src/androidTest/java/org/exoplatform/ApplicationTest.java
+++ app/src/androidTest/java/org/exoplatform/ApplicationTest.java
@@ -21,14 +21,12 @@ package org.exoplatform;
  */
 
 import android.app.Application;
-import android.test.ApplicationTestCase;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing
  * Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
+public class ApplicationTest  {
   public ApplicationTest() {
-    super(Application.class);
   }
 }
